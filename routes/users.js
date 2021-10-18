aa71acb5beb3742eb4f7838c2c9adaabeea3b88c
John Kim <cloudiosx@gmail.com>
2021-10-18 19:45:58 -0400

    Complete user authentication

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 const { check, validationResult } = require("express-validator");
 
 const db = require("../db/models");
@@ -8,14 +8,14 @@ const { csrfProtection, asyncHandler } = require("./utils");
 const { loginUser, logoutUser } = require("../auth");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
 });
 
-router.get('/signup', csrfProtection, (req, res) => {
-  const user = db.User.build()
-  res.render('user-signup', {
-    title: 'Sign-up',
+router.get("/signup", csrfProtection, (req, res) => {
+  const user = db.User.build();
+  res.render("user-signup", {
+    title: "Sign-up",
     user,
     csrfToken: req.csrfToken(),
   });
@@ -35,15 +35,13 @@ const userValidators = [
     .isEmail()
     .withMessage("Email Address is not a valid email")
     .custom((value) => {
-      return db.User.findOne({ where: { email: value } }).then(
-        (user) => {
-          if (user) {
-            return Promise.reject(
-              "The provided Email Address is already in use by another account"
-            );
-          }
+      return db.User.findOne({ where: { email: value } }).then((user) => {
+        if (user) {
+          return Promise.reject(
+            "The provided Email Address is already in use by another account"
+          );
         }
-      );
+      });
     }),
   check("password")
     .exists({ checkFalsy: true })
@@ -67,33 +65,96 @@ const userValidators = [
     }),
 ];
 
-router.post('/signup', csrfProtection, userValidators, asyncHandler(async(req, res) => {
-  const { userName, email, password } = req.body;
+router.post(
+  "/signup",
+  csrfProtection,
+  userValidators,
+  asyncHandler(async (req, res) => {
+    const { userName, email, password } = req.body;
 
-  const user = db.User.build({
-    userName,
-    email,
-  })
-
-  const validatorErrors = validationResult(req);
-
-  if(validatorErrors.isEmpty()) {
-    const hashedPassword = await bcrypt.hash(password, 10);
-    user.hashedPassword = hashedPassword;
-    await user.save();
-    loginUser(req, res, user);
-    res.redirect('/');
-  } else {
-    const errors = validatorErrors.array().map((error) => error.msg)
-    res.render('user-signup', {
-      title: 'Sign Up',
-      user,
-      errors,
-      csrfToken: req.csrfToken()
+    const user = db.User.build({
+      userName,
+      email,
     });
-  }
-}));
 
+    const validatorErrors = validationResult(req);
 
+    if (validatorErrors.isEmpty()) {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      user.hashedPassword = hashedPassword;
+      await user.save();
+      loginUser(req, res, user);
+      res.redirect("/");
+    } else {
+      const errors = validatorErrors.array().map((error) => error.msg);
+      res.render("user-signup", {
+        title: "Sign Up",
+        user,
+        errors,
+        csrfToken: req.csrfToken(),
+      });
+    }
+  })
+);
+
+router.get("/login", csrfProtection, (req, res) => {
+  const user = db.User.build();
+  res.render("user-login", {
+    title: "Login",
+    user,
+    csrfToken: req.csrfToken(),
+  });
+});
+
+const loginValidators = [
+  check("email")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a value for email address"),
+  check("password")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a value for password"),
+];
+
+router.post(
+  "/login",
+  csrfProtection,
+  loginValidators,
+  asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+    let errors = [];
+    const validationErrors = validationResult(req);
+    if (validationErrors.isEmpty()) {
+      const user = await db.User.findOne({ where: { email } });
+
+      if (user !== null) {
+        const passwordMatch = await bcrypt.compare(
+          password,
+          user.hashedPassword.toString()
+        );
+
+        if (passwordMatch) {
+          loginUser(req, res, user);
+          return res.redirect("/");
+        }
+      }
+
+      errors.push("Login failed for the provided email and password");
+    } else {
+      errors = validationErrors.array().map((error) => error.msg);
+    }
+
+    res.render("user-login", {
+      title: "Login",
+      email,
+      errors,
+      csrfToken: req.csrfToken(),
+    });
+  })
+);
+
+router.post("/logout", (req, res) => {
+  logoutUser(req, res);
+  res.redirect("/");
+});
 
 module.exports = router;

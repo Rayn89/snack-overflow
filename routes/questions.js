cc638a19b9071711639c6630315afe2b9c9eba70
Raymond Nehring <rnehring89@gmail.com>
2021-10-19 16:46:09 -0500

    Completed question ask/edit, working on delete

--- routes/questions.js
+++ routes/questions.js
@@ -5,7 +5,8 @@ const { check, validationResult } = require("express-validator");
 
 const db = require("../db/models");
 const { csrfProtection, asyncHandler } = require("./utils");
-const { loginUser, logoutUser } = require("../auth");
+const { requireAuth } = require("../auth");
+
 
 router.get('/',asyncHandler(async(req, res)=>{
     const questions = await db.Question.findAll({
@@ -19,6 +20,55 @@ router.get('/',asyncHandler(async(req, res)=>{
         questions
     })
 
+}));
+
+const contentValidator = [
+  check("content")
+    .exists({ checkFalsy: true })
+    .withMessage("Please fill in the form"),
+];
+
+const questionValidator = [
+    check('title')
+        .exists({checkFalsy: true})
+        .withMessage('Please provide a title')
+        .isLength({max: 255})
+        .withMessage('Title must not be more than 255 characters long'),
+    check("content")
+        .exists({ checkFalsy: true })
+        .withMessage("Please fill in the form"),
+]
+
+router.get('/ask', requireAuth, csrfProtection, asyncHandler(async(req, res) => {
+  const question = db.Question.build();
+  res.render("question-ask", {
+    // title: "",
+    question,
+    csrfToken: req.csrfToken(),
+  });
+}));
+
+router.post('/ask', requireAuth, csrfProtection, questionValidator, asyncHandler(async(req, res) => {
+    const {
+        title,
+        content,
+        userId
+    } = req.body;
+
+    const question = db.Question.build({title, content, userId})
+    const validatorErrors = validationResult(req);
+
+    if(validatorErrors.isEmpty()) {
+        await question.save();
+        res.redirect(`/questions/${question.id}`)
+    } else {
+        const errors = validatorErrors.array().map((error) => error.msg)
+        res.render('question-ask', {
+            question,
+            errors,
+            csrfToken: req.csrfToken(),
+        })
+    }
 }))
 
 router.get("/:id(\\d+)",asyncHandler(async(req, res)=>{
@@ -27,7 +77,53 @@ router.get("/:id(\\d+)",asyncHandler(async(req, res)=>{
         include: db.User
     })
     res.render("question",{question})
-}))
+}));
+
+router.get('/:id(\\d+)/edit', requireAuth, csrfProtection, asyncHandler(async(req, res) => {
+    const questionId = parseInt(req.params.id, 10);
+    const question = await db.Question.findByPk(questionId, {
+        include: db.User
+    })
+
+    if(res.locals.user.id !== question.userId) {
+        const err = new Error('Not authorized');
+        err.status = 401;
+        throw err
+    }
+    res.render("question-edit", {question, csrfToken: req.csrfToken()})
+}));
+
+router.post(
+  "/:id(\\d+)/edit",
+  requireAuth,
+  csrfProtection,
+  asyncHandler(async (req, res) => {
+    const questionId = parseInt(req.params.id, 10);
+    const question = await db.Question.findByPk(questionId, {
+      include: db.User,
+    });
+
+    const {
+        title,
+        content,
+    } = req.body;
+
+    const updatedQuestion = {
+        title,
+        content,
+    }
+
+    if (res.locals.user.id !== question.userId) {
+      const err = new Error("Not authorized");
+      err.status = 401;
+      throw err;
+    }
+    await question.update(updatedQuestion)
+    res.redirect(`/questions/${questionId}`)
+  })
+);
+
+router.get('/questions/')
 
 
 

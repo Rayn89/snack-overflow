cfc69d381a3fd94bd6ff9eafdcc7de58353c7884
Alfredo Sugawara <nicora221@gmail.com>
2021-10-20 21:03:37 -0400

    almost done

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,70 @@
-window.addEventListener("load", (event)=>{
-    console.log("hello from javascript!")
+window.addEventListener("DOMContentLoaded", ()=>{
+    // console.log("hello from javascript!")
+    function myAlert(){
+        alert("I am an alert box!")
+    }
+
+    let answers = document.querySelectorAll(".answerContainer")
+    // console.log(answers)
+
+    answers.forEach(answer =>{
+        const upButton = answer.querySelector(".up")
+        const voteCount = answer.querySelector(".count")
+        const downButton = answer.querySelector(".down")
+        console.log(answer)
+        console.log(upButton)
+        let count = voteCount.children[0]
+        let upped = false
+        let downed = false
+        upButton.addEventListener("click", async e =>{
+            const answerId = ( e.target.id ).split("-")[1]
+            // if(!upped){
+            //     upped = true
+            //     let currCount = Number(count.innerHTML)
+            //     currCount++
+            //     voteCount.children[0].innerHTML = currCount
+            // }else{
+            //     upped = false
+            //     let currCount = Number(count.innerHTML)
+            //     currCount--
+            //     voteCount.children[0].innerHTML = currCount
+            // }
+
+            const response = await fetch(`/vote/${answerId}/up`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+            })
+            const vote = await response.json()
+            console.log(vote)
+        })
+        downButton.addEventListener("click",async e=>{
+
+            // if(!downed){
+            //     downed = true
+            //     let currCount = Number(count.innerHTML)
+            //     currCount--
+            //     voteCount.children[0].innerHTML = currCount
+            // }else{
+            //     downed = false
+            //     let currCount = Number(count.innerHTML)
+            //     currCount++
+            //     voteCount.children[0].innerHTML = currCount
+            // }
+            const answerId = ( e.target.id ).split("-")[1]
+
+
+            try{const response = await fetch(`/vote/${answerId}/down`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+            })}catch(err){console.log(err)}
+            // const vote = await response.json()
+            // console.log(vote)
+        })
+
+
+    })
 })
--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ router.get('/', asyncHandler(async(req, res) => {
     const questions = await db.Question.findAll({
       include: db.User
     });
-    
+
     res.render("index", { questions });
 }));
 
@@ -20,7 +20,113 @@ router.get('/search', asyncHandler(async(req, res) => {
   res.render('search', {questions, term})
 }));
 
+router.post("/vote/:answerId/:type",asyncHandler(async (req, res)=>{
+  const type = req.params.type;
+  const answerId = req.params.answerId;
+  let answer = await db.Answer.findByPk(answerId);
+  let vote =await db.Vote.findOne({
+    where:{
+      answerId,
+      userId: req.session.auth.userId,
+    }
+  })
 
+  if(!vote){
+    vote = await db.Vote.create({
+      userId: req.session.auth.userId,
+      answerId,
+      up: type === "up" ? true : false,
+      down: type === "down" ? true : false
+    })
+    return res.json(vote)
+  }
+
+  if( type === "up"){
+
+    if(vote.down === true){
+      await answer.update({
+        answerScore:answer.answerScore+1
+      })
+      await vote.update({
+        down:false
+      })
+      // return res.json({...vote, answerScore : answer.answerScore})
+    }
+    //if vote.down === true then
+      //update answer.voteCount +1
+      //vote.down = false
+
+      console.log("answer:",answer.dataValues)
+      console.log("vote:",vote.dataValues)
+      if(vote.up === true){
+        await answer.update({
+          answerScore:answer.answerScore-1
+        })
+        await vote.update({
+          up:false
+        })
+        return res.json({ answerScore : answer.answerScore,...vote})
+      }
+
+      console.log("after:")
+      console.log("answer:",answer.dataValues)
+      console.log("vote:",vote.dataValues)
+
+    //if vote.up === true then
+      //decrement answer.voteCount
+      //update vote.up to be false
+
+    if(vote.up === false){
+      await answer.update({
+        answerScore:answer.answerScore+1
+      })
+      await vote.update({
+        up:true
+      })
+      return res.json({ answerScore : answer.answerScore,...vote})
+    }
+    //if vote.up === false then
+      //increment answer.voteCount
+      //update vote.up to be true
+
+  }else if(type === "down"){
+
+    if(vote.up === true){
+      console.log("downnn")
+      await answer.update({
+        answerScore:answer.answerScore-1
+      })
+      await vote.update({
+        up:false
+      })
+      // return res.json(vote)
+    }
+
+    if(vote.down === true){
+      await answer.update({
+        answerScore:answer.answerScore+1
+      })
+      await vote.update({
+        down:false
+      })
+      return res.json(vote)
+    }
+
+    if(vote.down === false){
+      await answer.update({
+        answerScore:answer.answerScore-1
+      })
+      await vote.update({
+        down:true
+      })
+      return res.json(vote)
+    }
+  }else{
+    return res.send('why?')
+  }
+
+  return res.json(vote)
+}))
 
 
 

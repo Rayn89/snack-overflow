9f8d6a55c02b0726712820fed11399601878b969
Alfredo Sugawara <nicora221@gmail.com>
2021-10-22 19:26:57 -0400

    fixed bugs + styling

--- app.js
+++ app.js
@@ -60,7 +60,11 @@ app.use(function (err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  if(err.status === 401){
+    res.render("401")
+  }else{
+    res.render("error");
+  }
 });
 
 module.exports = app;

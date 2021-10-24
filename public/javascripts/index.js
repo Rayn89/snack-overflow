cc886a459a247285d4ea0767a8ed064ea119248e
Alfredo Sugawara <nicora221@gmail.com>
2021-10-24 13:14:40 -0400

    vote button on load to display correct value

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -2,10 +2,47 @@ window.addEventListener("DOMContentLoaded", ()=>{
 
     let answers = document.querySelectorAll(".answerContainer")
 
+
     answers.forEach(answer =>{
         const upButton = answer.querySelector(".up")
         const voteCount = answer.querySelector(".count")
         const downButton = answer.querySelector(".down")
+        window.addEventListener("load", async e =>{
+            try{
+
+                const response = await fetch(
+
+                    `/vote/${answer.firstChild.firstChild.firstChild.id.split("-")[1]}/info`,
+                    {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                    }
+                )
+                const vote = await response.json()
+                console.log(vote)
+
+                voteCount.children[0].text = vote.answerScore
+
+                if(vote.up === true){
+                    upButton.classList.add("uptrue")
+                    downButton.classList.remove("downtrue")
+                } else if (vote.up === false){
+                    // console.log('upvote')
+                    upButton.classList.remove("uptrue")
+                }
+
+                if(vote.down === true){
+                    downButton.classList.add("downtrue")
+                    upButton.classList.remove("uptrue")
+                } else if(vote.down === false){
+                    downButton.classList.remove("downtrue")
+                }
+            } catch(err){
+                // console.log(err)
+            }
+        })
 
         upButton.addEventListener("click", async e =>{
             const answerId = ( e.target.id ).split("-")[1]
